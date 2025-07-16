a2aa011d31bb97a856f97f3128ea53756b1ec8d9
moneka <monekawilson16@gmail.com>
2025-07-16 13:00:26 +0300

    part1

--- OOP4/Program.cs
+++ OOP4/Program.cs
@@ -7,8 +7,37 @@ namespace OOP4
         static void Main(string[] args)
         {
             #region Q1
-            //
+            //c) To declare abstract methods and properties
             #endregion
+            #region Q2
+            //a) private
+            #endregion
+            #region Q3
+            //b) No
+            #endregion
+            #region Q4
+            //b) Yes, interfaces can inherit from multiple interfaces
+            #endregion
+            #region Q5
+            //d) implements
+            #endregion
+            #region Q6
+            //a) Yes
+            #endregion
+            #region Q7
+            //b) No, all members are implicitly public
+            #endregion
+            #region Q8
+            //a) To hide the interface members from outside access
+            #endregion
+            #region Q9
+            //No, interfaces cannot have constructors
+            #endregion
+            #region Q10
+            //c) By separating interface names with commas
+            #endregion
+            //part2
+
         }
     }
 }
