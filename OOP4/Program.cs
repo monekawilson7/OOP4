9bf13907f48f5c338871ba1e2f4695e8422f809a
moneka <monekawilson16@gmail.com>
2025-07-16 10:06:51 +0300

    Add project files.

---
+++ OOP4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //
+            #endregion
+        }
+    }
+}
